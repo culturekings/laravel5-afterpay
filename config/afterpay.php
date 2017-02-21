5aa7731579fd0bfe3cb86b20dd983e14008ea1ac
Jared Fraser <j.fraser@culturekings.com.au>
2017-02-21 13:04:45 +1000

    added new config options

--- config/afterpay.php
+++ config/afterpay.php
@@ -2,11 +2,13 @@
 
 return [
     'merchant' => [
+        'base_url' => env('AFTERPAY_MERCHANT_BASE_URL', \CultureKings\Afterpay\Model\Merchant\Authorization::SANDBOX_BASE_URL),
         'url' => env('AFTERPAY_API_URL', \CultureKings\Afterpay\Model\Merchant\Authorization::SANDBOX_URI),
         'merchantId' => env('AFTERPAY_MERCHANT_ID'),
         'secretKey' => env('AFTERPAY_SECRET_KEY'),
     ],
     'instore' => [
-        'url' => env('AFTERPAY_INSTORE_API_URL', \CultureKings\Afterpay\Model\InStore\Authorization::SANDBOX_URI)
-    ]
+        'url' => env('AFTERPAY_INSTORE_API_URL', \CultureKings\Afterpay\Model\InStore\Authorization::SANDBOX_URI),
+        'base_url' => env('AFTERPAY_INSTORE_API_BASE_URL', \CultureKings\Afterpay\Model\InStore\Authorization::SANDBOX_BASE_URI),
+    ],
 ];
